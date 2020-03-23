7425c5f26cfcaf74cfe2fc4c8fcd1754f4afc338
maeliss vns <maeliss.avenoso@gmail.com>
2020-03-23 11:59:02 +0100

    add sign-in form and custom button

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './sign-in-sign-up.scss';
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => (
+    <SignIn/>
+);
+
+export default SignInAndSignUpPage;
