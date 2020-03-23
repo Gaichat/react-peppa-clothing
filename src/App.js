81c109d23ebb1082c307dd2bf4f97c12247d191e
maeliss vns <maeliss.avenoso@gmail.com>
2020-03-23 19:19:51 +0100

    add sign in with email and pwd, add sign up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/shop.component';
 import HatsPage from "./pages/hatspage/hatspage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./components/firebase/firebase.util"
+import { auth, createUserProfileDocument } from "./firebase/firebase.util"
 
 class App extends React.Component {
 
@@ -18,14 +18,39 @@ class App extends React.Component {
     unsubscribeFroAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFroAuth = auth.onAuthStateChanged( user => {
-                this.setState( {currentUser: user}) ;
-                console.log(user);
+        //async because request to our firestore
+        /*
+        Whenever we call the onAuthStateChanged() or onSnapshot() methods from our auth  library or referenceObject,
+         we get back a function that lets us unsubscribe from the listener we just instantiated.
+         */
+        this.unsubscribeFroAuth = auth.onAuthStateChanged( async userAuth => {
+               // this.setState( {currentUser: user}) ;
+                //createUserProfileDocument(user);
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                //
+                userRef.onSnapshot( snapshot => {
+                    this.setState({
+                        currentUser: ({
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        })
+                    }, () => console.log(this.state)); ///setState is async we need to use a callback
+                });
+            }else{
+                this.setState({currentUser: userAuth});
+            }
             }
         );
     }
 
     componentWillUnmount() {
+        /*
+        Calling the unsubscribe function when the component is about to unmount is the best way to
+        make sure we don't get any memory leaks in our application related to listeners still being open
+         even if the component that cares about the listener is no longer on the page.
+         */
         this.unsubscribeFroAuth = null;
     }
 
